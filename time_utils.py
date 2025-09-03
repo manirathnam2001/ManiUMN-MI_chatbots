a0c9f41b75ae3e9e8118b0c77c8a2b36175269a4
manirathnam2001 <117564491+manirathnam2001@users.noreply.github.com>
2025-09-03 14:37:42 -0500

    Change UTC time format to MM-DD-YYYY

--- time_utils.py
+++ time_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
+from time_utils import get_formatted_utc_time
 
 def get_formatted_utc_time():
-    """Returns current UTC time in YYYY-MM-DD HH:MM:SS format"""
-    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+    """Returns current UTC time in MM-DD-YYYY HH:MM:SS format"""
+    return datetime.utcnow().strftime("%m-%d-%y %H:%M:%S")
